9dcf784e87b394b3ff373e8be9726a615646a193
Sagar Subedi <sagarxic@gmail.com>
2025-04-02 20:17:05 +0545

    removed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { CoursePage } from './pages/CoursePage'
 

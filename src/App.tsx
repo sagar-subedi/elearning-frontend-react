bbcd2d1f92ef76d6be34609bb1c19c3d5b3487d9
Sagar Subedi <sagarxic@gmail.com>
2025-05-11 12:52:37 +0545

    Implement route guards

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { CoursePage } from './pages/CoursePage'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { CoursePage } from "./pages/CoursePage";
+import { Login } from "./pages/Login";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AuthProvider } from "./context/AuthContext";
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/courses/:courseId" element={<CoursePage />} />
-        </Routes>
-    </BrowserRouter>
-    </>
-  )
-}
-
-export default App
+export const App: React.FC = () => {
+    return (
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route
+                        path="/courses/:courseId"
+                        element={
+                            <ProtectedRoute>
+                                <CoursePage />
+                            </ProtectedRoute>
+                        }
+                    />
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
+};
